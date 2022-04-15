83a04a1cb8da15aa06611db631e54e90533fc33f
Rémi PHILIPPOT <rphilippot@icloud.com>
2022-04-15 11:31:23 +0200

    connect to MongoDBAtlas

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
-
+// const databaseUrl =
+//   process.env.DATABASE_URL;
+//   console.log(process.env.DATABASE_URL);
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    console.log(process.env.DATABASE_URL);
+    await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
     throw new Error(error)
   }
 }
+
